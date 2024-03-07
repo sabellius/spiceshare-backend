b0a0de3066b36d806311f54daa6db44f20a79b43
Saveliy Shiryaev <thatisme128@gmail.com>
2024-03-07 14:30:07 +0200

    add crud methods for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: %i[create show update]
+      resources :recipes, only: %i[index create update show destroy]
     end
   end
 end
