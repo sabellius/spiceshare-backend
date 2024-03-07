dc877da94fe3cb0b1f57706919ed49518a5a9637
Saveliy Shiryaev <thatisme128@gmail.com>
2024-03-07 13:48:07 +0200

    seed recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,35 @@
   first_name = Faker::Name.unique.first_name
   last_name = Faker::Name.last_name
   User.create!(
-    first_name: first_name,
-    last_name: last_name,
+    first_name:,
+    last_name:,
     username: Faker::Internet.unique.username,
     email: Faker::Internet.email(name: "#{first_name} #{last_name}", separators: ['.'], domain: 'example.com'),
     password: Faker::Internet.password(min_length: 8, special_characters: true)
   )
+end
+
+recipes = File.read(Rails.root.join('db/seeds/recipes.json'))
+parsed_recipes = JSON.parse(recipes, object_class: OpenStruct)
+parsed_recipes.each do |recipe|
+  recipe_object = Recipe.create!(
+    title: recipe.title,
+    description: recipe.description,
+    prep_time: recipe.prep_time,
+    cook_time: recipe.cook_time,
+    servings: recipe.servings,
+    user: User.all.sample
+  )
+  recipe.ingredients.each do |i|
+    recipe_object.ingredients << Ingredient.create!(
+      name: i.name,
+      amount: i.amount,
+      unit: i.unit,
+      optional: false,
+      recipe: recipe_object
+    )
+  end
+  recipe.instructions.each_with_index do |i, index|
+    recipe_object.instructions << Instruction.create!(step_number: index + 1, description: i, recipe: recipe_object)
+  end
 end
